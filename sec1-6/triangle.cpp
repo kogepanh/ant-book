fe6aa1e3fd047fa1706f43f6a36d91a4ea91dff3
kogepanh <k.hosono1209@gmail.com>
2020-11-20 22:12:52 +0900

    init

---
+++ sec1-6/triangle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin >> n;
+    set<int> a;
+    int tmp;
+    for(int i=0; i<n; i++) cin >> tmp; a.insert(tmp);
+    
+    return 0;
+}
