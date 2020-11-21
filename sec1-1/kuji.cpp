c8cc733448e9637cde323c9471f3e496d64d048c
kogepanh <k.hosono1209@gmail.com>
2020-11-21 14:25:05 +0900

    edit

--- sec1-1/kuji.cpp
+++ sec1-1/kuji.cpp
@@ -2,6 +2,25 @@
 using namespace std;
  
 int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> k(n);
+    for(int i=0; i<n; i++) cin >> k[i];
+
+    for(int a=0; a<n; a++){
+        for(int b=0; b<n; b++){
+            for(int c=0; c<n; c++){
+                for(int d=0; d<n; d++){
+                    if(k[a] + k[b] + k[c] + k[d] == m){
+                        cout << "Yes" << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "No" << endl;
 
     return 0;
 }
