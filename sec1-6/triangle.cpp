a42be1ebbae2d42006842d43f0c7d282cc04f7d0
kogepanh <k.hosono1209@gmail.com>
2020-11-21 10:50:02 +0900

    edit

--- sec1-6/triangle.cpp
+++ sec1-6/triangle.cpp
@@ -4,9 +4,19 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    set<int> a;
-    int tmp;
-    for(int i=0; i<n; i++) cin >> tmp; a.insert(tmp);
-    
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    for(int i=0; i<n-2; i++){
+        if(a[i] < a[i+1] + a[i+2]){
+            cout << (a[i] + a[i+1] + a[i+2]) << endl;
+            return 0;
+        }
+    }
+
+    cout << "0" << endl;
+
     return 0;
 }
