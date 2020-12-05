7d1a2f5fe6ee4eda70f50371ba0d5df77fbcfc47
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:54:30 +0900

    edit

--- sec1-1/kuji.cpp
+++ sec1-1/kuji.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
+
+int main()
+{
     int n, m;
     cin >> n >> m;
     vector<int> k(n);
-    for(int i=0; i<n; i++) cin >> k[i];
+    for (int i = 0; i < n; i++)
+        cin >> k[i];
 
-    for(int a=0; a<n; a++){
-        for(int b=0; b<n; b++){
-            for(int c=0; c<n; c++){
-                for(int d=0; d<n; d++){
-                    if(k[a] + k[b] + k[c] + k[d] == m){
+    for (int a = 0; a < n; a++)
+    {
+        for (int b = 0; b < n; b++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                for (int d = 0; d < n; d++)
+                {
+                    if (k[a] + k[b] + k[c] + k[d] == m)
+                    {
                         cout << "Yes" << endl;
                         return 0;
                     }
