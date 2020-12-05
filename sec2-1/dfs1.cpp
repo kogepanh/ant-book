fb9288dab05097099694f8d36209c065d33c545b
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:55:18 +0900

    edit

--- sec2-1/dfs1.cpp
+++ sec2-1/dfs1.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dfs(int i){
+bool dfs(int i)
+{
 }
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
 
     vector<int> a(n);
-    for(int i=0; i<n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
     int k;
     cin >> k;
--- sec2-1/ex.cpp
+++ sec2-1/ex.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 // 階乗
-int fact(int n){
-    if(n == 0) return 1;
-    return n * fact(n-1);
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    return n * fact(n - 1);
 }
 
 // フィボナッチ数列
-int fib(int n){
-    if(n <= 1) return n;
-    return fib(n-1) + fib(n-2);
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
 }
 
-int main(){
+int main()
+{
     // stack
     stack<int> s;
     s.push(1);
